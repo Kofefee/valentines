2eaeeec3459accffb0fe2cb11908f7ecad745d71
Felix <felix.koelle@hs-furtwangen.de>
2026-02-03 20:29:01 +0100

    myValentine

--- action.js
+++ action.js
@@ -4,17 +4,28 @@ const yesBtn = document.getElementById("yes");
 const noBtn = document.getElementById("no");
 const title = document.getElementById("title");
 
+
+const gifWaiting = document.getElementById("gif-waiting");
+const gifYes = document.getElementById("gif-yes");
+
+
 noBtn.addEventListener("click", () => {
     size += 6;
     yesBtn.style.fontSize = size + "px";
 
-    // kleine Animation
+    gifWaiting.style.display = "block";
+    gifYes.style.display = "none";
+
     yesBtn.classList.add("wiggle");
     setTimeout(() => yesBtn.classList.remove("wiggle"), 300);
 });
 
 yesBtn.addEventListener("click", () => {
     title.innerHTML = "YAAAY 💕🥰 I knew it!";
+
+    gifWaiting.style.display = "none";
+    gifYes.style.display = "block";
+
     noBtn.style.display = "none";
     yesBtn.style.transform = "scale(1.3)";
 });
